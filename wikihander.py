44811e122a2fca3d431e344da69f1472b236c530
aaron <aaron@bigrig.localdomain>
2013-03-18 11:29:47 -0400

    added method to base handler class for setting user cookie and copied utility.py from GAE wiki app

--- wikihander.py
+++ wikihander.py
@@ -13,6 +13,12 @@ class Handler:
     template = jinja_env.get_template(template)
     print template.render(**kw)
 
+  def set_user_cookie(self, val):
+    name = 'user_id'
+    secret_key = make_secure_val(val)
+    web.config.session_parameters['cookie_name'] = name
+    web.config.session_parameters['secret_key'] = secret_key
+
 class index:
   def GET(self):
     return "Hello, World!"
@@ -32,3 +38,5 @@ urls = (
 if __name__ == "__main__":
   app = web.application(urls, globals())
   app.run()
+  app = web.application(urls, locals())
+  session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'count': 0})
